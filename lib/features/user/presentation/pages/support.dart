8099cde64d356ce069758b6889cce579565f9f8a
Isaiah-code <amomensahisaiah@gmail.com>
2024-08-28 20:34:28 +0000

    rf

--- lib/features/user/presentation/pages/support.dart
+++ lib/features/user/presentation/pages/support.dart
@@ -3,9 +3,6 @@ import 'package:go_router/go_router.dart';
 import 'package:rideme_mobile/core/enums/profile_item_type.dart';
 import 'package:rideme_mobile/core/extensions/context_extensions.dart';
 import 'package:rideme_mobile/core/size/sizes.dart';
-import 'package:rideme_mobile/core/spacing/whitspacing.dart';
-import 'package:rideme_mobile/features/trips/domain/entities/create_trip_info.dart';
-import 'package:rideme_mobile/features/trips/presentation/widgets/pricing_card.dart';
 import 'package:rideme_mobile/features/user/presentation/widgets/profile_item_listing_widget.dart';
 
 class SupportPage extends StatefulWidget {
@@ -20,8 +17,6 @@ class _SupportPageState extends State<SupportPage> {
     ProfileItemType.faq,
     ProfileItemType.safety,
   ];
-
-  bool tester = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -52,24 +47,6 @@ class _SupportPageState extends State<SupportPage> {
                 );
               },
             ),
-
-            //TODO: TO BE CLEARED; TESTING WIDGET BUILD UP
-            Space.height(context, 0.05),
-            PricingCard(
-              isSelected: tester,
-              onTap: () => setState(() {
-                tester = !tester;
-              }),
-              pricing: const Pricing(
-                  id: 1,
-                  charge: 22,
-                  isAvailable: true,
-                  vehicleType: 'suv',
-                  expiration: 'expiration',
-                  description: '15 min away',
-                  cost: 34,
-                  tag: 'economy'),
-            )
           ],
         ),
       ),
