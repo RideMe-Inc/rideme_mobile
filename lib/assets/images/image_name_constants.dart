25117d1daa5ca4ededcfb4b0964bea5d36e3a712
Isaiah-code <amomensahisaiah@gmail.com>
2024-08-31 21:35:55 +0000

    asset addition

--- lib/assets/images/image_name_constants.dart
+++ lib/assets/images/image_name_constants.dart
@@ -7,7 +7,12 @@ class ImageNameConstants {
   static const onboardingIMG2 = '$_baseUrl/onboarding2.png';
   static const onboardingIMG3 = '$_baseUrl/onboarding3.png';
   static const driverIMG = '$_baseUrl/driver.png';
-  static const carIMG = '$_baseUrl/car.png';
+  static const carIMG = '$_baseUrl/car_new1.png';
+  static const economyIMG = '$_baseUrl/economy.png';
+  static const electricIMG = '$_baseUrl/electric.png';
+  static const hybridIMG = '$_baseUrl/hybrid.png';
+  static const suvIMG = '$_baseUrl/suv.png';
+  static const premiumIMG = '$_baseUrl/premium.png';
   static const ambulanceIMG = '$_baseUrl/ambulance.png';
   static const cashIMG = '$_baseUrl/cash.png';
   static const cardIMG = '$_baseUrl/card.png';
@@ -17,4 +22,5 @@ class ImageNameConstants {
   static const ridemeLogoIMG = '$_baseUrl/rideme_logo.png';
   static const locationPinIMG = '$_baseUrl/location_pin.png';
   static const noTripsImagePinIMG = '$_baseUrl/no_trips_image.png';
+  static const noDriverIMG = '$_baseUrl/no_driver.png';
 }
